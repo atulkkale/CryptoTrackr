af6286d5fd381b7b562a8c9471d7d06096266cbb
Atul Kale <kaleatul9@gmail.com>
2025-01-20 22:57:32 +0530

    Dashboard added.

---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,7 @@
+import SectionWrapper from "../components/SectionWrapper/SectionWrapper";
+
+const Dashboard: React.FC = () => {
+  return <SectionWrapper sectionClass="dashboard">Dashboard</SectionWrapper>;
+};
+
+export default Dashboard;
