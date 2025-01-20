d89ca38d3d6134409792df8734b53ce8a12e92b6
Atul Kale <kaleatul9@gmail.com>
2025-01-20 22:57:39 +0530

    History page added.

---
+++ src/views/History.tsx
@@ -0,0 +1,7 @@
+import SectionWrapper from "../components/SectionWrapper/SectionWrapper";
+
+const History: React.FC = () => {
+  return <SectionWrapper sectionClass="dashboard">History</SectionWrapper>;
+};
+
+export default History;
