66f7bd4f3c88ffdc460fce0a017df3c919122efa
Atul Kale <kaleatul9@gmail.com>
2025-01-20 22:57:47 +0530

    Overview page added.

---
+++ src/views/OverView.tsx
@@ -0,0 +1,7 @@
+import SectionWrapper from "../components/SectionWrapper/SectionWrapper";
+
+const OverView: React.FC = () => {
+  return <SectionWrapper sectionClass="dashboard">OverView</SectionWrapper>;
+};
+
+export default OverView;
