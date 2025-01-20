cd38bd0374d33736ae5d7b93f1c7d7d9b27f899e
Atul Kale <kaleatul9@gmail.com>
2025-01-20 22:57:01 +0530

    Footer added.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import SectionWrapper from "../SectionWrapper/SectionWrapper";
+
+const Footer: React.FC = () => {
+  return (
+    <SectionWrapper tag="footer" sectionClass="footer">
+      This is the footer.
+    </SectionWrapper>
+  );
+};
+
+export default Footer;
