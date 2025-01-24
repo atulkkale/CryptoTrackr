ddfbbb3d4fc37edcb4175e2e02949293101761d4
Atul Kale <kaleatul9@gmail.com>
2025-01-24 21:00:44 +0530

    Added overview page functionality.

--- src/components/OverViewInfo/OverViewInfo.tsx
+++ src/components/OverViewInfo/OverViewInfo.tsx
@@ -1,11 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+
+import { fetchAssetById } from "../../api/cryptoApi";
+import { RootState } from "../../store/store";
+
 const OverViewInfo: React.FC = () => {
+  const selectedCrypto = useSelector(
+    (state: RootState) => state.crypto.activeCrypto
+  );
+
+  const { data } = useQuery({
+    queryKey: ["asset", selectedCrypto],
+    queryFn: () => fetchAssetById(selectedCrypto!),
+    enabled: !!selectedCrypto,
+    refetchInterval: 5000,
+  });
+
   return (
     <ul>
-      <li>Market Cap - </li>
-      <li>Total Supply - </li>
-      <li>Circulating Supply - </li>
-      <li>All-time High price - </li>
-      <li>Rank - </li>
+      <li>Market Cap - {parseFloat(data?.marketCapUsd!).toFixed(2)}</li>
+      <li>Total Supply - {parseFloat(data?.maxSupply!).toFixed(2)}</li>
+      <li>Circulating Supply - {parseFloat(data?.supply!).toFixed(2)}</li>
+      <li>All-time High price - {parseFloat(data?.priceUsd!).toFixed(2)}</li>
+      <li>Rank - {parseFloat(data?.rank!).toFixed(2)}</li>
     </ul>
   );
 };
