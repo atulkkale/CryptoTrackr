27e6c0cb4843673de20c50cc5ef30eab63b4c99f
Atul Kale <kaleatul9@gmail.com>
2025-01-25 20:41:35 +0530

    Added crypto images.

--- src/components/CurrentPriceDisplay/CurrentPriceDisplay.tsx
+++ src/components/CurrentPriceDisplay/CurrentPriceDisplay.tsx
@@ -1,14 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-import './CurrentPriceDisplay.css';
+import "./CurrentPriceDisplay.css";
 
-import { RootState } from '../../store/store';
-import { fetchAssetById } from '../../api/cryptoApi';
-import { updateLastFetchTime } from '../../store/cryptoSlice';
-import { getCurrentTime, getRandomPriceCardColor } from '@utils/util';
-import LivePrice from '@components/LivePrice/LivePrice';
+import { RootState } from "../../store/store";
+import { fetchAssetById } from "../../api/cryptoApi";
+import { updateLastFetchTime } from "../../store/cryptoSlice";
+import { getCurrentTime, getRandomPriceCardColor } from "@utils/util";
+import LivePrice from "@components/LivePrice/LivePrice";
+import bitCoinImg from "../../assets/bitcoin.svg";
+import tetherImg from "../../assets/tether.svg";
+import cardanoImg from "../../assets/cardano.svg";
+import binanceImg from "../../assets/binance.svg";
 
 const CurrentPriceDisplay: React.FC = () => {
   const dispatch = useDispatch();
@@ -17,14 +21,14 @@ const CurrentPriceDisplay: React.FC = () => {
   );
 
   const { data, fetchStatus } = useQuery({
-    queryKey: ['asset', selectedCrypto],
+    queryKey: ["asset", selectedCrypto],
     queryFn: () => fetchAssetById(selectedCrypto!),
     enabled: !!selectedCrypto,
     refetchInterval: 5000,
   });
 
   useEffect(() => {
-    if (fetchStatus === 'fetching') {
+    if (fetchStatus === "fetching") {
       dispatch(updateLastFetchTime(getCurrentTime()));
     }
   }, [fetchStatus]);
@@ -36,23 +40,33 @@ const CurrentPriceDisplay: React.FC = () => {
         style={{ backgroundColor: getRandomPriceCardColor() }}
       >
         <h2>{data!.name}</h2>
-        <img
-          src="https://criptic.vercel.app/_next/static/media/bitcoin.81bd702b.svg"
-          alt={data!.name}
-        />
+        <ul className="crypto-images">
+          <li>
+            <img src={bitCoinImg} alt={data!.name} />
+          </li>
+          <li>
+            <img src={tetherImg} alt={data!.name} />
+          </li>
+          <li>
+            <img src={cardanoImg} alt={data!.name} />
+          </li>
+          <li>
+            <img src={binanceImg} alt={data!.name} />
+          </li>
+        </ul>
         <div className="coin-price">
           <LivePrice id={selectedCrypto!} /> {data!.symbol}
         </div>
-        <ul>
+        <ul className="price-percentage-info">
           <li className="price">{parseFloat(data!.priceUsd).toFixed(2)} USD</li>
           <li
             className={
               parseFloat(data!.changePercent24Hr) > 0
-                ? 'percentage-positive'
-                : 'percentage-negative'
+                ? "percentage-positive"
+                : "percentage-negative"
             }
           >
-            {parseFloat(data!.changePercent24Hr) > 0 ? '↑ +' : '↓ '}
+            {parseFloat(data!.changePercent24Hr) > 0 ? "↑ +" : "↓ "}
             {parseFloat(data!.changePercent24Hr).toFixed(2)}
           </li>
         </ul>
