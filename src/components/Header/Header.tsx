950d20fe75c317321b23fb742ca578ab62d6a3bc
Atul Kale <kaleatul9@gmail.com>
2025-01-25 16:53:06 +0530

    Removed unnecessary code.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import "./Header.css";
 
-import { NavLink } from "react-router-dom";
 import SectionWrapper from "../SectionWrapper/SectionWrapper";
 import SelectedCrypto from "../SelectCrypto/SelectCrypto";
 import HeadingTitle from "@components/HeadingTitle/HeadingTitle";
