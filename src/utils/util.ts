a92c0ae859b2e4ed6944987f14a6a6924e8af0c0
Atul Kale <kaleatul9@gmail.com>
2025-01-25 16:53:48 +0530

    Added random color functionality.

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -6,3 +6,8 @@ export const getCurrentTime = (): string => {
     second: "2-digit",
   });
 };
+
+export const getRandomPriceCardColor = (): string => {
+  const colors = ["#FDEDD4", "#E1F9F1", "#DBE3FF", "FBF5D5"];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
