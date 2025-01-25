ec9b81bfdd39768f3c3c9ce44c63a5b06e741254
Atul Kale <kaleatul9@gmail.com>
2025-01-25 21:01:37 +0530

    Updated chart colors.

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -27,6 +27,24 @@ ChartJS.register(
   Legend
 );
 
+const barColors = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(153, 102, 255, 0.2)",
+  "rgba(255, 159, 64, 0.2)",
+];
+
+const barBorderColors = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+];
+
 const BarChart: React.FC = () => {
   const dispatch = useDispatch();
   const selectedCryptos = useSelector(
@@ -53,8 +71,8 @@ const BarChart: React.FC = () => {
       {
         label: "24h Percentage Change",
         data: changePercent24Hr,
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: barColors.slice(0, selectedCryptos.length),
+        borderColor: barBorderColors.slice(0, selectedCryptos.length),
         borderWidth: 1,
       },
     ],
